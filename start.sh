2025c340249728285cac0c38e6b9135d043553d9
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2017-10-16 18:51:44 +0200

    new version 0.0.2 for SDK6.0.0

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 APPID=ti.infoline
-VERSION=0.0.1
+VERSION=0.0.2
 
 export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk1.8.0_65.jdk/Contents/Home/"
 
@@ -9,5 +9,4 @@ cd android && rm -rf build/classes && \
 rm -f "~/Library/Application Support/Titanium/modules/android/$APPID/$VERSION/lib/*" && \
 ti --platform android build --build-only --sdk 6.2.2.GA && \
 unzip -uo dist/$APPID-android-$VERSION.zip  -d  ~/Library/Application\ Support/Titanium/ && \
-unzip -uo dist/$APPID-android-$VERSION.zip -d ~/Documents/MLearning/mobilelearningapp &&\
 cd ..
